d2473c186869a3f3bb9143bc7ae69c1f4252a6fa
Martin Ashby <martin@ashbysoft.com>
2018-09-04 21:16:48 +0100

    Started replacing custom server with loopback.io

--- sailracetimerserver/src/main/kotlin/net/mfashby/sailracetimerserver/Entities.kt
+++
@@ -1,7 +0,0 @@
-package net.mfashby.sailracetimerserver
-import io.ktor.auth.Principal
-
-data class User(val id: Int? = null,
-                val name: String,
-                val password: String,
-                val level: Int): Principal
