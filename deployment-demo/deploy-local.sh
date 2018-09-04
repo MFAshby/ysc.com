64f2ab4efedef2861cd977377adc9bcbe64dd367
Martin Ashby <martin@ashbysoft.com>
2018-09-04 23:09:57 +0100

    Switched to yarn for installing server Modified deploy-local script to install server packages

--- deployment-demo/deploy-local.sh
+++ deployment-demo/deploy-local.sh
@@ -10,16 +10,22 @@ export DB_PASSWORD=example
 export REACT_APP_API_SERVER="http://localhost:8080/api"
 export PUBLIC_URL="http://localhost:8080/results"
 
-# Build the results widget
-pushd ../sailraceresults
+# Install the server node_modules
+pushd ../sailraceserver
 yarn
-  yarn run build
 popd
 
-# Copy build products to the static web server folder
+# Install results widget node_modules & build it
+pushd ../sailraceresults
+yarn
+yarn run build
+popd
+
+# Copy results widget to the static web server folder
 rm -rf front/static/results
 cp -r ../sailraceresults/build front/static/results
 
+# rebuild all docker images, re-create containers
 docker-compose down
 docker-compose build
 docker-compose up -d
