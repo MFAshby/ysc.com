dfb431866f9477638cdf1357d1de64ec13028628
Martin Ashby <martin@ashbysoft.com>
2018-09-04 19:42:02 +0100

    Fixed local deployment of demo for new structure

--- deployment-demo/deploy-local.sh
+++ deployment-demo/deploy-local.sh
@@ -6,19 +6,19 @@
 git submodule update --init --recursive --remote
 
 # Build the server project
-pushd sailracetimerserver
+pushd ../sailracetimerserver
 ./gradlew clean build fatJar
 popd
 
 # Build the results widget
-pushd sailraceresults
-npm install
-REACT_APP_API_SERVER="http://localhost:8080/api" PUBLIC_URL="http://localhost:8080/results" npm run build
+pushd ../sailraceresults
+yarn
+REACT_APP_API_SERVER="http://localhost:8080/api" PUBLIC_URL="http://localhost:8080/results" yarn run build
 popd
 
 # Copy build products to the static web server folder
 rm -rf front/static/results
-cp -r sailraceresults/build front/static/results
+cp -r ../sailraceresults/build front/static/results
 
 docker-compose down
 docker-compose build
