4e8c2d6e714a85f7ccdf4547fcb37b24bd8805ab
Martin Ashby <martin@ashbysoft.com>
2018-09-16 22:36:09 +0100

    Changed urls for prod deployment

--- deployment/deploy-prod.sh
+++ deployment/deploy-prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://raw.githubusercontent.com/MFAshby/ysc.com/master/deployment/docker-compose.yml
-wget https://raw.githubusercontent.com/MFAshby/ysc.com/master/deployment/docker-compose.prod.yml
+wget https://rawgit.com/MFAshby/ysc.com/master/deployment/docker-compose.yml
+wget https://rawgit.com/MFAshby/ysc.com/master/deployment/docker-compose.prod.yml
 docker-compose up -f docker-compose.yml -f docker-compose.prod.yml -d
 docker-compose logs -f
