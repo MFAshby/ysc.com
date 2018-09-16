9772f2b9bbe31382943054c18bc1c10f432e4f6f
Martin Ashby <martin@ashbysoft.com>
2018-09-16 22:28:51 +0100

    Production deployment script

--- deployment/deploy-prod.sh
+++ deployment/deploy-prod.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-# Download the main docker-compose file and the production one
+wget https://raw.githubusercontent.com/MFAshby/ysc.com/master/deployment/docker-compose.yml
+wget https://raw.githubusercontent.com/MFAshby/ysc.com/master/deployment/docker-compose.prod.yml
+docker-compose up -f docker-compose.yml -f docker-compose.prod.yml -d
+docker-compose logs -f
