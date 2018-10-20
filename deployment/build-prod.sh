777c3b4e0d0df16a3a23b1d70dea8f99c34f6530
Martin Ashby <martin@ashbysoft.com>
2018-10-20 22:54:19 +0100

    Add series component to prod build

--- deployment/build-prod.sh
+++ deployment/build-prod.sh
@@ -11,6 +11,12 @@ yarn
 yarn run build
 popd
 
+# Build series component
+pushd ../sailraceseries
+yarn
+yarn run build
+popd
+
 # Build the input component
 pushd ../sailraceinput
 yarn
@@ -22,6 +28,7 @@ rm -rf wordpress/dist/sailraceresults/
 mkdir -p wordpress/dist/sailraceresults/
 cp -r ../sailraceresults/dist/. wordpress/dist/sailraceresults/results
 cp -r ../sailraceinput/dist/. wordpress/dist/sailraceresults/input
+cp -r ../sailraceseries/dist/. wordpress/dist/sailraceresults/series
 cp wordpress/sailraceresults.php wordpress/dist/sailraceresults/
 
 # Build the docker images (this embeds all the files in them)
