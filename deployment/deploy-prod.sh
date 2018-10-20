770901038f374d10a2c85dad0116f1f83c74c207
Martin Ashby <martin@ashbysoft.com>
2018-10-20 23:18:10 +0100

    Added image pull to prod deployment

--- deployment/deploy-prod.sh
+++ deployment/deploy-prod.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+docker-compose pull -f docker-compose.yml -f docker-compose.prod.yml -d
 docker-compose up -f docker-compose.yml -f docker-compose.prod.yml -d
 docker-compose logs -f
