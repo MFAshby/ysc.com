a2560cc7bda8337d9cf48f4bc7d59dcb76cfe072
Martin Ashby <martin@ashbysoft.com>
2018-10-20 22:19:09 +0100

    Removed wordpress standard files from repo

--- deployment/build-prod.sh
+++ deployment/build-prod.sh
@@ -17,17 +17,12 @@ yarn
 yarn run build
 popd
 
-# Copy single page apps to web server static files dir
-rm -rf front/static/results
-cp -r ../sailraceresults/results front/static/results
-rm -rf front/static/input
-cp -r ../sailraceinput/dist front/static/input
-
-# Copy single page apps to wordpress as well
-rm -rf wordpress/files/html/wp-content/plugins/sailraceresults/results
-cp -r ../sailraceresults/results/. wordpress/files/html/wp-content/plugins/sailraceresults/results
-rm -rf wordpress/files/html/wp-content/plugins/sailraceresults/input
-cp -r ../sailraceinput/dist/. wordpress/files/html/wp-content/plugins/sailraceresults/input
+# Copy single page apps to wordpress
+rm -rf wordpress/dist/sailraceresults/
+mkdir -p wordpress/dist/sailraceresults/
+cp -r ../sailraceresults/dist/. wordpress/dist/sailraceresults/results
+cp -r ../sailraceinput/dist/. wordpress/dist/sailraceresults/input
+cp wordpress/sailraceresults.php wordpress/dist/sailraceresults/
 
 # Build the docker images (this embeds all the files in them)
 docker-compose -f docker-compose.yml -f docker-compose.build.yml build
