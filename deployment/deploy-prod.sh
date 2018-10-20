1e0349ca1f57bacf8222ca98ecaf71e7b009f4cf
Martin Ashby <martin@ashbysoft.com>
2018-10-20 23:16:25 +0100

    Fiddling with production deployment

--- deployment/deploy-prod.sh
+++ deployment/deploy-prod.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-
-wget https://rawgit.com/MFAshby/ysc.com/master/deployment/docker-compose.yml
-wget https://rawgit.com/MFAshby/ysc.com/master/deployment/docker-compose.prod.yml
 docker-compose up -f docker-compose.yml -f docker-compose.prod.yml -d
 docker-compose logs -f
